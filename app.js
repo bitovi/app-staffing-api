b0d777e66734c85af282dfef0faf1b5e8a8ff664
Michael Haynie <michael.dean.haynie@gmail.com>
2021-09-29 13:39:10 -0700

    remove fluff

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 'use strict'
 
-const path = require('path')
-const AutoLoad = require('fastify-autoload')
+const fastify = require('fastify')({
+  logger: true
+});
+const APP_PORT = process.env.APP_PORT || 3000;
 
-module.exports = async function (fastify, opts) {
-  // Place here your custom code!
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
+});
 
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
-
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'routes'),
-    options: Object.assign({}, opts)
-  })
-}
+// Run the server!
+fastify.listen(APP_PORT, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+  console.log(`Server is now listening on ${address}`);
+});

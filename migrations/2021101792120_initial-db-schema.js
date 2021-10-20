d69aafcf730716ecf4975e86dd07710958db2009
Michael Haynie <michael.dean.haynie@gmail.com>
2021-10-20 09:00:10 -0700

    add simple initial db migration (#14)

---
+++ migrations/2021101792120_initial-db-schema.js
@@ -0,0 +1,10 @@
+exports.up = async (knex) => {
+    await knex.schema.createTable('skill', function (table) {
+      table.string('id').notNullable()
+      table.integer('name').notNullable()
+    })
+  }
+  
+  exports.down = async (knex) => {
+    await knex.schema.dropTable('name')
+  }

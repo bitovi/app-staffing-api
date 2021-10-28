c2f72c0b3a23980da9eee6053ad77d0be8b47e65
Kevin Phillips <kevin@bitovi.com>
2021-10-27 21:19:02 -0500

    refactoring app.js to make testing easier

--- src/app.js
+++ src/app.js
@@ -1,22 +1,5 @@
 'use strict'
 
-const fastify = require('fastify')({
-  logger: true
-})
-const APP_PORT = process.env.APP_PORT || 3000
+const { start } = require('./server')
 
-// Declare a route
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
-})
-
-// Run the server!
-// Host '0.0.0.0' so that docker networking works
-fastify.listen(APP_PORT, '0.0.0.0', function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-
-  console.log(`Server is now listening on ${address}`)
-})
+start()
---
+++ src/server.js
@@ -0,0 +1,31 @@
+const fastify = require('fastify')({
+  logger: true
+})
+const APP_PORT = process.env.APP_PORT || 3000
+
+const start = () => {
+  // Declare a route
+  fastify.get('/', (request, reply) => {
+    reply.send({ hello: 'world' })
+  })
+
+  // Run the server!
+  // Host '0.0.0.0' so that docker networking works
+  return fastify.listen(APP_PORT, '0.0.0.0')
+    .then(address => {
+      console.log(`Server is now listening on ${address}`)
+    })
+    .catch(err => {
+      fastify.log.error(err)
+      process.exit(1)
+    })
+}
+
+const stop = () => {
+  return fastify.close()
+}
+
+module.exports = {
+  start,
+  stop
+}

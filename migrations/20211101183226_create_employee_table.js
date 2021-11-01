2668f7e5e116166eca79b9226c212a51c2f6fe70
Cherif BOUCHELAGHEM <cherif.bouchelaghem@gmail.com>
2021-11-01 19:22:25 +0100

    Employee model, migration and seeding

---
+++ migrations/20211101183226_create_employee_table.js
@@ -0,0 +1,15 @@
+exports.up = async (knex) => {
+    await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "public"');
+    await knex.schema.createTable('employee', (table) => {
+        table.uuid('id')
+            .defaultTo(knex.raw('uuid_generate_v4()'))
+            .primary();
+        table.string('name').notNullable();
+        table.date('start_date').nullable();
+        table.date('end_date').nullable();
+    });
+};
+
+exports.down = async (knex) => {
+    await knex.schema.dropTable('employee');
+};

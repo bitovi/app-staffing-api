ec22080849727f431c5d831447268fe6f3b53772
Marshall Thompson <marshall@creativeideal.net>
2021-11-01 12:17:21 -0600

    Use uuid

--- migrations/20211101113758_projects.js
+++ migrations/20211101113758_projects.js
@@ -1,6 +1,6 @@
 exports.up = async (knex) => {
   await knex.schema.createTable("project", function (table) {
-    table.increments("id");
+    table.uuid("id").primary();
     table.string("name").notNullable();
     table.date("start_date").notNullable();
     table.date("end_date");

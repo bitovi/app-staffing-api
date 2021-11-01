f4cd46fadc79cda0af2c0d0a5ec4a9bd01195c12
Stephen Couturier <stephen@bitovi.com>
2021-11-01 17:02:39 -0400

    Removed duplicate code chunk

--- src/models/employee.js
+++ src/models/employee.js
@@ -45,23 +45,3 @@ module.exports = class Employee extends Model {
     }
   }
 }
-
-  static get relationMappings () {
-    const Skill = require('./skill')
-
-    return {
-      skills: {
-        relation: Model.ManyToManyRelation,
-        modelClass: Skill,
-        join: {
-          from: 'employee.id',
-          through: {
-            from: 'employee__skill.employee_id',
-            to: 'employee__skill.skill_id'
-          },
-          to: 'skill.id'
-        }
-      }
-    }
-  }
-}

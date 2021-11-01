84fa6e74d9b7dfee4a691586c62ee5e4728abcc5
Marshall Thompson <marshall@creativeideal.net>
2021-11-01 14:40:51 -0600

    fixes n stuff

--- db/migrations/20211101103316_roles_table_creation.js
+++ db/migrations/20211101103316_roles_table_creation.js
@@ -1,17 +1,17 @@
 exports.up = async (knex) => {
-  await knex.raw(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
-  await knex.schema.createTable("role", function (table) {
-    table.uuid("id")
+  await knex.raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+  await knex.schema.createTable('role', function (table) {
+    table.uuid('id')
       .defaultTo(knex.raw('uuid_generate_v4()'))
-      .primary();
-    table.date("start_date");
-    table.integer('start_confidence');
-    table.date("end_date")
-    table.integer('end_confidence');
-    table.integer("project_id").notNullable();
-  });
-};
+      .primary()
+    table.date('start_date')
+    table.integer('start_confidence')
+    table.date('end_date')
+    table.integer('end_confidence')
+    table.integer('project_id').notNullable()
+  })
+}
 
 exports.down = async (knex) => {
-  await knex.schema.dropTable("role");
-};
+  await knex.schema.dropTable('role')
+}
--- db/migrations/20211101140103_create_employee__skill.js
+++ db/migrations/20211101140103_create_employee__skill.js
@@ -1,11 +1,11 @@
 exports.up = async (knex) => {
-  await knex.schema.createTable("employee__skill", (table) => {
-    table.uuid("employee_id");
-    table.uuid("skill_id");
-    table.primary(["employee_id", "skill_id"]);
-  });
-};
+  await knex.schema.createTable('employee__skill', (table) => {
+    table.uuid('employee_id')
+    table.uuid('skill_id')
+    table.primary(['employee_id', 'skill_id'])
+  })
+}
 
 exports.down = async (knex) => {
-  await knex.schema.dropTable("employee__skill");
-};
+  await knex.schema.dropTable('employee__skill')
+}

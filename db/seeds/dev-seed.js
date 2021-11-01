ec1d3251595c2a6bd4cec5d31ba12384ea4caeff
Marshall Thompson <marshall@creativeideal.net>
2021-11-01 14:20:22 -0600

    employee__skill ftw

--- db/seeds/dev-seed.js
+++ db/seeds/dev-seed.js
@@ -1,51 +1,65 @@
-const { Model } = require('objection')
-const Project = require('../../models/project')
-const faker = require('faker')
+const { Model } = require("objection");
+const Employee = require("../../src/models/employee");
+const faker = require("faker");
 
-const fakesCache = new Map()
-function generateAndCacheFake (keyPrefix, key, generator) {
-  const prefixedKey = `${keyPrefix}.${key}`
-  let value
+const fakesCache = new Map();
+function generateAndCacheFake(keyPrefix, key, generator) {
+  const prefixedKey = `${keyPrefix}.${key}`;
+  let value;
 
   // retrieve value from cache by key (if provided)
   if (key) {
-    value = fakesCache.get(prefixedKey)
+    value = fakesCache.get(prefixedKey);
   }
 
   // generate fake value via provided generator (if needed)
   if (!value) {
-    value = generator()
+    value = generator();
   }
 
   // cache value by key (if provided)
   if (key) {
-    fakesCache.set(prefixedKey, value)
+    fakesCache.set(prefixedKey, value);
   }
 
-  return value
+  return value;
 }
 
-function fakeSkill (key) {
-  return generateAndCacheFake('skill', key, () => `${faker.random.word().toLowerCase()}.js`)
+function fakeSkill(key) {
+  return generateAndCacheFake(
+    "skill",
+    key,
+    () => `${faker.random.word().toLowerCase()}.js`
+  );
 }
 
-function fakeEmployee (key) {
-  return generateAndCacheFake('employee', key, () => faker.fake('{{name.firstName}} {{name.lastName}}'))
+function fakeEmployee(key) {
+  return generateAndCacheFake("employee", key, () =>
+    faker.fake("{{name.firstName}} {{name.lastName}}")
+  );
 }
 
-function fakeProject (key) {
-  return generateAndCacheFake('project', key, () => faker.company.companyName())
+function fakeProject(key) {
+  return generateAndCacheFake("project", key, () =>
+    faker.company.companyName()
+  );
 }
 
 exports.seed = async (knex) => {
   // Give the knex instance to Objection
-  Model.knex(knex)
+  Model.knex(knex);
 
   // delete data in reverse dependency order to avoid fk issues
-  await knex('role').del()
-  await knex('project').del()
-  await knex('employee').del()
-  await knex('skill').del()
+  await knex("role").del();
+  await knex("project").del();
+  await knex("employee").del();
+  await knex("skill").del();
 
   // insert seed data
-}
+  await Employee.query().insertGraph({
+    name: "Michael Scott",
+    start_date: new Date(),
+    end_date: new Date(),
+    skills: [{ name: "Node" }, { name: "Angular" }, { name: "React" }],
+  });
+};

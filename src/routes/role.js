493d7eb501fc5fad3eeab26fd1da613756957629
Ben Baker <bitoviben@Bens-MacBook-Pro.local>
2021-11-02 13:54:40 -0500

    added role routes

---
+++ src/routes/role.js
@@ -0,0 +1,84 @@
+const JSONAPISerializer = require('json-api-serializer')
+
+const Serializer = new JSONAPISerializer({
+ convertCase: 'kebab-case'
+})
+
+const RolesModel = require('../models/role')
+
+Serializer.register('role', {
+  id: 'id',
+  start_date: 'start_date',
+  start_confidence: 'start_confidence',
+  end_date: 'end_date',
+  end_confidence: 'end_confidence',
+  role_id: 'role_id',
+})
+
+const routes = {
+   create: {
+     method: 'POST',
+     url: '/roles',
+     schema: {
+       body: RolesModel.getSchema
+     },
+     handler: async function (request, reply) {
+       const { body, url } = request
+       if (body.id) reply.send().status(403)
+       const newRole = await RolesModel.query().insert(body)
+       const data = Serializer.serialize('roles', newRole)
+       const location = `${url}/${newRole.id}`
+       reply.status(201).header('Location', location).send(data)
+     }
+   },
+   list: {
+     method: 'GET',
+     url: '/roles',
+     handler: async function (_, reply) {
+       const roles = await RolesModel.query()
+       const data = Serializer.serialize('roles', roles.map(role => role.toJSON()))
+       reply.send(data)
+     }
+   },
+   get: {
+     method: 'GET',
+     url: '/roles/:id',
+     handler: async function (request, reply) {
+       const id = request.params.id
+       try {
+         const role = await RolesModel.query().findById(id)
+         const data = Serializer.serialize('roles', role.toJSON())
+         reply.send(data)
+       } catch (e) {
+         reply.status(404).send()
+       }
+     }
+   },
+   update: {
+     method: 'PATCH',
+     url: '/roles/:id',
+     handler: async function (request, reply) {
+       const id = request.params.id
+       const { body } = request
+       try {
+         const role = await RolesModel.query().patchAndFetchById(id, body)
+         const data = Serializer.serialize('roles', role.toJSON())
+         reply.send(data)
+       } catch (e) {
+         reply.status(404).send()
+       }
+     }
+   },
+   delete: {
+     method: 'DELETE',
+     url: '/roles/:id',
+     handler: async function (request, reply) {
+       const id = request.params.id
+       const numberDeleted = await RolesModel.query().deleteById(id)
+       const status = numberDeleted > 0 ? 204 : 404
+       reply.status(status).send()
+     }
+   }
+ }
+
+ module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
+const knexConfigFile = "./knexfile.js";
+const knexConfig = require(knexConfigFile);
+
+const Knex = require("knex");
+const { Model } = require("objection");
+const knex = Knex(knexConfig);
+Model.knex(knex);
+
 const fastify = require('fastify')({
   logger: true
 })
 const APP_PORT = process.env.APP_PORT || 3000
+
+const projectRoutes = require('./routes/role.js');
 
 const start = () => {
   // Declare a route
   fastify.get('/', (request, reply) => {
     reply.send({ hello: 'world' })
   })
+
+  for (const routeKey in projectRoutes) {
+    fastify.route(projectRoutes[routeKey])
+  }
 
   // Run the server!
   // Host '0.0.0.0' so that docker networking works

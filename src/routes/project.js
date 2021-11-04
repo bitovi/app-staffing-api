a61efee6f63c724a086cbfc03d5b0ffde4a65dc2
Roye <rxa1967@yum.com>
2021-11-04 12:29:12 -0400

    Remove custom error handling

--- src/routes/project.js
+++ src/routes/project.js
@@ -11,12 +11,15 @@ const routes = {
     },
     handler: async function (request, reply) {
       const { body, url } = request
-      if (body.id) reply.send().status(403)
+
+      if (body.id) {
+        reply.send().status(403)
+      }
 
       const newProject = await ProjectModel.query().insert(body)
       const data = Serializer.serialize('projects', newProject)
       const location = `${url}/${newProject.id}`
-      reply.status(201).header('Location', location).send(data)
+      return reply.status(201).header('Location', location).send(data)
     }
   },
   list: {
@@ -26,51 +29,39 @@ const routes = {
       const includeStr = getIncludeStr(request.query)
       const projects = await ProjectModel.query().withGraphFetched(includeStr)
       const data = Serializer.serialize('projects', projects.map(project => project.toJSON()))
-      reply.send(data)
+      return reply.send(data)
     }
   },
   get: {
     method: 'GET',
     url: '/projects/:id',
     handler: async function (request, reply) {
-      const id = request.params.id
-      try {
-        const includeStr = getIncludeStr(request.query)
-        const project = await ProjectModel.query().findById(id).withGraphFetched(includeStr)
+      const includeStr = getIncludeStr(request.query)
+      const project = await ProjectModel.query().findById(request.params.id).withGraphFetched(includeStr)
 
-        if (!project) {
-          return reply.status(404).send()
-        }
-
-        const data = Serializer.serialize('projects', project.toJSON())
-        return reply.send(data)
-      } catch (e) {
-        return reply.status(500).send()
+      if (!project) {
+        return reply.status(404).send()
       }
+
+      const data = Serializer.serialize('projects', project.toJSON())
+      return reply.send(data)
     }
   },
   update: {
     method: 'PATCH',
     url: '/projects/:id',
     handler: async function (request, reply) {
-      try {
-        await ProjectModel.query().upsertGraph(request.body, { insertMissing: true, update: false })
-        reply.send()
-      } catch (e) {
-        console.log(e)
-        reply.status(500).send()
-      }
+      await ProjectModel.query().upsertGraph(request.body, { insertMissing: true, update: false })
+      return reply.send()
     }
   },
   delete: {
     method: 'DELETE',
     url: '/projects/:id',
     handler: async function (request, reply) {
-      const id = request.params.id
-
-      const numberDeleted = await ProjectModel.query().deleteById(id)
+      const numberDeleted = await ProjectModel.query().deleteById(request.params.id)
       const status = numberDeleted > 0 ? 204 : 404
-      reply.status(status).send()
+      return reply.status(status).send()
     }
   }
 }

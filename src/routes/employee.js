9a0fd83d7fe0996d09d253d34733aae5ad01d3ed
Marshall Thompson <marshall@creativeideal.net>
2021-11-04 15:52:59 -0600

    sorting, pagination, filtering on Employes

--- src/routes/employee.js
+++ src/routes/employee.js
@@ -1,15 +1,52 @@
 const Employee = require('../models/employee')
 const { Serializer } = require('../json-api-serializer')
+const createError = require('http-errors')
 const { getIncludeStr } = require('../utils')
+
+const tableName = 'employee'
+const normalizeColumn = column => column.includes('.') ? column : `${tableName}.${column}`
 
 module.exports = {
   list: {
     url: '/employees',
     method: 'GET',
     async handler (request, reply) {
+      // Sort
+      const orderBy = request.query.sort?.split(',').map(column =>
+        column.startsWith('-')
+          ? { column: normalizeColumn(column.substr(1)), order: 'DESC' }
+          : { column: normalizeColumn(column) })
+
+      // Paginate
+      const page = request.query['page[number]'] || 0
+      const pageSize = request.query['page[size]'] || 20
+
       const includeStr = getIncludeStr(request.query)
-      const data = await Employee.query().withGraphFetched(includeStr)
-      const result = Serializer.serialize('employees', data, {
+      const dbQuery = Employee.query()
+        .orderBy(orderBy || [])
+        .page(page, pageSize)
+        .withGraphJoined(includeStr)
+
+      // Return error if trying to filter a value with an array
+      const filteredByArray = Object.entries(request.query.filter || {}).find(([key, value]) => Array.isArray(value))
+      if (filteredByArray) return reply.send(createError(400, `cannot have multiple values for filter property '${filteredByArray[0]}'`))
+
+      // Clean Up Filters
+      const filterBy = Object.entries(request.query)
+        .filter(([key]) => key.startsWith('filter'))
+        .map(([key, value]) => {
+          const plainKey = key.substr(7).split(']')[0]
+          return { key: plainKey.includes('.') ? plainKey : `employee.${plainKey}`, value }
+        })
+
+      // Filter
+      filterBy.forEach(({ key, value }) => {
+        const safeValue = value.toLowerCase().split('%').join('\\%')
+        dbQuery.where(key, 'ilike', `%${safeValue}%`)
+      })
+
+      const data = await dbQuery.debug()
+      const result = Serializer.serialize('employees', data.results, {
         count: data.length
       })
       reply.send(result)

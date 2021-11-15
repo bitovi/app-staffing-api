6a0ac8b34aac8421a480c0d74c6868d8a18b326a
abpin <kjoseph.us@gmail.com>
2021-11-15 21:11:09 +0200

    roled back changes

---
+++ db/migrations/20211101103316_roles_table_creation.js
@@ -0,0 +1,16 @@
+exports.up = async (knex) => {
+  await knex.schema.createTable('role', function (table) {
+    table.uuid('id')
+      .defaultTo(knex.raw('uuid_generate_v4()'))
+      .primary()
+    table.timestamp('start_date')
+    table.integer('start_confidence')
+    table.timestamp('end_date')
+    table.integer('end_confidence')
+    table.uuid('project_id').notNullable()
+  })
+}
+
+exports.down = async (knex) => {
+  await knex.schema.dropTable('role')
+}

bd6c2302048e7ea63bb9da4ed04e2bff32028e06
BS <bens640@gmail.com>
2022-05-10 17:23:22 -0400

    Added comments

--- src/models/assignment.js
+++ src/models/assignment.js
@@ -58,16 +58,25 @@ module.exports = class Assignment extends Model {
     const role = await Role.query().findById(body.role_id).select('start_date', 'end_date')
     const assignmentStart = new Date(body.start_date)
     const assignmentEnd = new Date(body.end_date)
+    /*
+    This if statement checks for the following scenarios:
+    1. If there is no end date in assignment and the assignment start date is outside of the role's start and end date
+    2. If there is no end date in role and the assignment start or end date is before the role's start date
+    3. If both the role and assignment end date are not null and assignment start is before role start or assignment end is after role end
+     If any of these scenarios are true, throw a Validation Error
+     */
     if (
+      // 1
       (body.end_date === null && (assignmentStart < role.start_date || assignmentStart > role.end_date)) ||
+      // 2
       (role.end_date === null && (assignmentStart < role.start_date || assignmentEnd < role.start_date)) ||
+      // 3
       ((body.end_date !== null && role.end_date !== null) &&
         (assignmentStart < role.start_date || assignmentEnd > role.end_date))) {
       throw new ValidationError({
         message: 'Assignment not in date range of role',
         type: 'ModelValidation',
-        statusCode: 409,
-        data: ''
+        statusCode: 409
       })
     }
   }

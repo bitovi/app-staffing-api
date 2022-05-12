4e8fdc44c572b766689e1d6c16e91541055cca08
BS <bens640@gmail.com>
2022-05-12 13:02:12 -0400

    changed utils to date-utils, refactored dateGenerator, added dateGenerator to all relevant tests

--- src/utils/date-utils.js
+++ src/utils/date-utils.js
@@ -2,7 +2,7 @@ const faker = require('faker')
 /*
 Function that returns an Object that includes a range of dates as follows:
   beforeStartDate->startDate->startBeforeAssignmentDate->endAssignmentDate->endAfterAssignmentDate->endDate->afterEndDate
-* */
+*/
 const dateGenerator = function () {
   // ref is used in start to ensure that startDate and endDate will never overlap
   const ref = new Date('2020-01-01')
@@ -12,7 +12,7 @@ const dateGenerator = function () {
   const midpoint = faker.date.between(start, end)
   const startAssignmentDate = faker.date.between(start, midpoint)
   const endAssignmentDate = faker.date.between(midpoint, end)
-  // Formats the date to 'YYY-MM-DD' and if the offset parameter is passed in, adds the offset to the day to avoid overlap
+  // Formats the date to 'YYYY-MM-DD' and if the offset parameter is passed in, adds the offset to the day to avoid overlap
   const toDateFormat = (date, offset = false) => {
     return `${date.getFullYear() + 1}-${date.getMonth() + 1}-${offset ? date.getDay() + offset : date.getDay() + 1}`
   }

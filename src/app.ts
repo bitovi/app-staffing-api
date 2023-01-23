66bf5683a4ed67f9cdc1e340ad3da1aebd666f46
George Ijidola <geordola@gmail.com>
2023-01-23 17:00:12 +0100

    Add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
-import { createStaffingAppInstance } from "./server";
+import { createStaffingAppInstance } from './server'
 
 async function init() {
-  const [app, scaffold] = await createStaffingAppInstance();
+  const [app, scaffold] = await createStaffingAppInstance()
 
-  await scaffold.createDatabase();
+  await scaffold.createDatabase()
+
+  app.on('error', (err, ctx) => {
+    console.error('server error', err, ctx)
+  })
 
   app.listen(3000, () => {
-    console.log("Scaffold Started");
-  });
+    console.log('Scaffold Started')
+  })
 }
 
-init();
+init()
---
+++ src/managers/error-handler/index.js
@@ -0,0 +1,54 @@
+const {
+    ValidationError,
+    NotFoundError,
+    DBError,
+    ConstraintViolationError,
+    UniqueViolationError,
+    NotNullViolationError,
+    ForeignKeyViolationError,
+    CheckViolationError,
+    DataError
+} = require('objection')
+const { statusCodes } = require('./constants')
+const ajvValidationErrorHandlers = require('./types/ajv-validation-errors')
+const databaseErrorHandlers = require('./types/database-errors')
+const generalErrorHandlers = require('./types/general-errors')
+
+const errorHandler = function (error, _, reply) {
+    let errors = []
+    let status = statusCodes.INTERNAL_SERVER_ERROR
+
+    if (error.validation && error.validationContext) {
+        errors = ajvValidationErrorHandlers(error)
+
+        status = errors.length > 1 ? statusCodes.BAD_REQUEST : errors[0].status
+    } else if (
+        [
+            ValidationError,
+            NotFoundError,
+            DBError,
+            ConstraintViolationError,
+            UniqueViolationError,
+            NotNullViolationError,
+            ForeignKeyViolationError,
+            CheckViolationError,
+            DataError
+        ].includes(error.constructor)
+    ) {
+        errors.push(databaseErrorHandlers(error))
+
+        status = errors[0].status
+    } else {
+        errors.push(generalErrorHandlers(error))
+
+        status = errors[0].status
+    }
+
+    console.log(error)
+
+    reply.status(status).send({
+        errors
+    })
+}
+
+module.exports = errorHandler

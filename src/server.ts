72f270d3f53a5661d1415c69a32104bdacdf739b
George Ijidola <geordola@gmail.com>
2023-02-24 19:51:54 +0100

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,28 @@
-import { Scaffold } from "bitscaffold";
-import Koa from "koa";
-import signale from "signale";
-import KoaRouter from "@koa/router";
-import dotenv from 'dotenv';
+import { Scaffold } from 'bitscaffold'
+import Koa from 'koa'
+import signale from 'signale'
+import KoaRouter from '@koa/router'
+import dotenv from 'dotenv'
+import cors from '@koa/cors'
 
-import { Assignment } from "./models/Assignment";
-import { Employee } from "./models/Employee";
-import { Project } from "./models/Project";
-import { Role } from "./models/Role";
-import { Skill } from "./models/Skill";
+import { Assignment } from './models/Assignment'
+import { Employee } from './models/Employee'
+import { Project } from './models/Project'
+import { Role } from './models/Role'
+import { Skill } from './models/Skill'
 
-dotenv.config();
+dotenv.config()
 
 export function createStaffingAppInstance(): [Koa, Scaffold] {
   // Create a basic Koa application
-  const app = new Koa();
-  const router = new KoaRouter();
+  const app = new Koa()
+  const router = new KoaRouter()
+
+  app.use(cors())
 
   // Create a Scaffold instance containing your Models
   const scaffold = new Scaffold([Assignment, Employee, Project, Role, Skill], {
-    prefix: "/api",
+    prefix: '/api',
     expose: true,
     database: {
       dialect: 'postgres',
@@ -28,27 +31,27 @@ export function createStaffingAppInstance(): [Koa, Scaffold] {
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      logging: false,
+      logging: false
     }
-  });
+  })
 
   // Set up your Koa app as normal, for example, a logging middleware
   app.use(async (ctx, next) => {
-    signale.info("Incoming Request: ", ctx.method, ctx.path);
-    await next();
-  });
+    signale.info('Incoming Request: ', ctx.method, ctx.path)
+    await next()
+  })
 
   // Hook up the router
-  app.use(router.routes());
-  app.use(router.allowedMethods());
+  app.use(router.routes())
+  app.use(router.allowedMethods())
 
   // Attach the Scaffold default middleware to your Koa application
-  app.use(scaffold.middleware.allModels.all);
+  app.use(scaffold.middleware.allModels.all)
 
   // Set up any other Koa routes, middleware, etc, that you want.
   app.use(async (ctx) => {
-    ctx.body = { response: "up" };
-  });
+    ctx.body = { response: 'up' }
+  })
 
-  return [app, scaffold];
+  return [app, scaffold]
 }

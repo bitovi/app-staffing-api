d1b55cc4602eeb2dc31e6384d4fe4c96ea334e2a
RoyAyo <46534625+RoyAyo@users.noreply.github.com>
2023-04-06 13:01:13 +0100

    Feat/scaffold/seed dev (#143)
    * creating seeding data
    
    * Added the seed data

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import { createStaffingAppInstance } from './server'
+import seedData from './db/seed';
 
 async function init() {
   const [app, scaffold] = await createStaffingAppInstance()
 
-  await scaffold.createDatabase()
+  await scaffold.createDatabase();
+
+  await seedData(scaffold);
 
   app.listen(3000, () => {
     console.log('Scaffold Started')

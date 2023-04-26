361616591681cbd414856d9902b4992485734968
Roy <roylayindeay0@gmail.com>
2023-04-26 19:12:39 +0100

    fixed the error

--- src/server.ts
+++ src/server.ts
@@ -28,15 +28,13 @@ export function createStaffingAppInstance(): [Koa, Scaffold] {
     prefix: '/api',
     expose: true,
     database: {
-      // dialect: 'postgres',
-      // host: process.env.DB_HOST ?? 'localhost',
-      // port: Number(process.env.DB_PORT) ?? 5432,
-      // username: process.env.DB_USERNAME,
-      // password: process.env.DB_PASSWORD,
-      // database: process.env.DB_NAME,
-      // logging: false,
-      dialect: 'sqlite',
-      storage: 'random.sqlite'
+      dialect: 'postgres',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: Number(process.env.DB_PORT) ?? 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      logging: false
     }
   })
 
